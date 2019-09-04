2162926f2573f23ecd56300edf57f413b3208d70
Jan Leeuwerink <jan.leeuwerink@upcmail.nl>
2019-09-04 22:04:09 +0200

    updates

--- assets/js/coveytodo.js
+++ assets/js/coveytodo.js
@@ -1,5 +1,14 @@
 console.log("connected");
 
+var varr_kwadrant = [
+    "Belangrijk-Urgent", "Belangrijk-Niet Urgent",
+    "Niet Belangrijk-Urgent",
+    "Niet Belangrijk-Niet Urgent"
+];
+
+// Which kwadrant is selected
+var vKwadrant = 1;
+
 if (jQuery) {
     console.log("jQuery loaded");
 } else {
@@ -13,6 +22,14 @@ $("ul").on("click", "li", function() {
     // console.log("li clicked");
     $(this).toggleClass("taskcompleted");
 });
+
+// Add a listener on the Kwadrant selection
+
+$("#sel-kwadrant").change(function(event) {
+    console.log($(this).val());
+    vKwadrant = Number($(this).val());
+});
+
 
 // Add a listener on the Trashcan
 
@@ -31,10 +48,28 @@ function f_Trashcan_listener() {
 $("input[type='text']").keypress(function(event) {
     // console.log("Key pressed");
     if (event.which === 13) {
-        // console.log($(this).val());
+        console.log($(this).val());
+        console.log(vKwadrant);
         var vTodotext = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span><i class=\"far fa-trash-alt\"></i></span>" + vTodotext + "</li>");
+        // Which kwadrant is selected
+        if (vKwadrant === 1 ) {
+            console.log("Kwadrant1");
+            $("#kwadrant1").append("<li><span><i class=\"far fa-trash-alt\"></i></span>" + vTodotext + "</li>");
+
+        } else if (vKwadrant === 2) {
+            console.log("Kwadrant2");
+            $("#kwadrant2").append("<li><span><i class=\"far fa-trash-alt\"></i></span>" + vTodotext + "</li>");
+
+        } else if (vKwadrant === 3) {
+            console.log("Kwadrant3");
+            $("#kwadrant3").append("<li><span><i class=\"far fa-trash-alt\"></i></span>" + vTodotext + "</li>");
+
+        } else if (vKwadrant === 4) {
+            console.log("Kwadrant4");
+            $("#kwadrant4").append("<li><span><i class=\"far fa-trash-alt\"></i></span>" + vTodotext + "</li>");
+
+        };
         f_Trashcan_listener();
     };
 });
